0c3e4831ed3bf629d2aea732325bba0976943ffa
Chrisyhjiang <chrisyh.jiang@gmail.com>
2023-07-23 09:33:40 -0400

    add the constant class

---
+++ src/constants.h
@@ -0,0 +1,14 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <string>
+using namespace std;
+
+const int MAX_LEVEL = 5;
+const string SHADE = "Shade";
+const string DROW = "Drow";
+const string VAMPIRE = "Vampire";
+const string TROLL = "Troll";
+const string GOBLIN = "Goblin";
+
+#endif
