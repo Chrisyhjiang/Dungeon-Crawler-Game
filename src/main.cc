9749b0e701b6ee70527072b761d55b6ef5acb377
Chrisyhjiang <chrisyh.jiang@gmail.com>
2023-07-29 15:11:48 -0400

    add..

--- src/main.cc
+++ src/main.cc
@@ -9,9 +9,9 @@ int main(int argc, char* argv[]) {
     srand(static_cast<unsigned int>(time(0)));
     ChamberCrawler *game = new ChamberCrawler();
     if(argc > 1){
-        game->start(argv[1]);
+        game->start(argv[1], true);
     }else{
-         game->start("");
+         game->start("", true);
     }
     delete game;
    
